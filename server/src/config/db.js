ac2f7ef5b202d9c119effafc47f3d707065ec822
liton-das <litonchandra094@gmail.com>
2025-12-12 22:33:09 +0600

    updated

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,12 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnection =async()=>{
     try {
-        await mongoose.connect(`${process.env.DB_CONNECT}`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.83ortx4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        )
         console.log('db connected')
     } catch (error) {
         console.log('db not connected!')
