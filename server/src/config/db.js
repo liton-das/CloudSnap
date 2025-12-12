24c9e26ba86f96a32521ec8a40f122171737d98d
liton-das <litonchandra094@gmail.com>
2025-12-12 22:36:25 +0600

    change db connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,12 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnection =async()=>{
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.83ortx4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
-        )
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.83ortx4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
         console.log('db connected')
     } catch (error) {
         console.log('db not connected!')
