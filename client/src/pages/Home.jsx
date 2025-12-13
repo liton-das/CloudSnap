665591b7e79ddc2f9f9326dd6c4476cdd890b440
liton-das <litonchandra094@gmail.com>
2025-12-13 08:39:37 +0600

    work with client home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -98,7 +98,7 @@ const Home = () => {
                       className="w-full h-full object-contain"
                     />
                     <button
-                      onClick={handleRemoveImg}
+                      onClick={handleRemove}
                       className="absolute bottom-4 right-4 px-4 py-2 rounded-lg bg-red-500 text-white text-sm shadow"
                     >
                       Remove
